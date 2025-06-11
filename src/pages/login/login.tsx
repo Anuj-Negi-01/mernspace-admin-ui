ac395068314bee843772dd9908f4b0e18c3e7113
AnujNegi007 <anujnegi708@gmail.com>
2025-06-11 15:45:17 +0530

    first component test

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,15 @@
+function LoginPage() {
+  return (
+    <>
+      <h1>Sign in</h1>
+      <input type="text" name="" id="" placeholder="username"/>
+      <input type="text" name="" id="" placeholder="password"/>
+      <button>Login</button>
+      <label htmlFor="remeber-me">Remember me</label>
+      <input type="checkbox" name="Remember me" id="remeber-me" />
+      <a href="#">Forgot password</a>
+    </>
+  )
+}
+
+export default LoginPage
