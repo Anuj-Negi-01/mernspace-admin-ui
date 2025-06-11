349501ec1a0d564b4f72f19227de24088bf1372d
AnujNegi007 <anujnegi708@gmail.com>
2025-06-11 20:16:58 +0530

    add login form

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,15 +1,67 @@
+import { Checkbox, Form, Input, Layout, Space, Button, Flex } from "antd";
+import { Card } from "antd";
+import { LockFilled, UserOutlined, LockOutlined } from "@ant-design/icons";
+import Logo from "../../components/icons/Logo";
+
 function LoginPage() {
   return (
     <>
-      <h1>Sign in</h1>
-      <input type="text" name="" id="" placeholder="username"/>
-      <input type="text" name="" id="" placeholder="password"/>
-      <button>Login</button>
-      <label htmlFor="remeber-me">Remember me</label>
-      <input type="checkbox" name="Remember me" id="remeber-me" />
-      <a href="#">Forgot password</a>
+      <Layout
+        style={{ height: "100vh", display: "grid", placeItems: "center" }}
+      >
+        <Space direction="vertical" align="center" size="large">
+          <Layout.Content style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <Logo/>
+        </Layout.Content>
+        <Card
+          title={
+            <Space
+              style={{ width: "100%", fontSize: 16, justifyContent: "center" }}
+            >
+              <LockFilled />
+              Sign in
+            </Space>
+          }
+          variant="borderless"
+          style={{ width: 300 }}
+        >
+          <Form initialValues={{ "remember": true}}>
+            <Form.Item name="username" rules={[
+              {
+                required: true,
+                message: 'Please enter username'
+              },{
+                type: 'email',
+                message: 'Email is not valid'
+              }
+            ]}>
+              <Input prefix={<UserOutlined />} placeholder="username" />
+            </Form.Item>
+            <Form.Item name="password" rules={[
+              {
+                required: true,
+                message: 'Please enter password'
+              }
+            ]}>
+              <Input.Password prefix={<LockOutlined />} placeholder="password" />
+            </Form.Item>
+            <Flex justify="space-between">
+              <Form.Item name="remember" valuePropName="checked">
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+              <a href="" id="login-form-forgot">Forgot password</a>
+            </Flex>
+            <Form.Item>
+              <Button type='primary' htmlType="submit" style={{ width: '100%'}}>
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+        </Space>
+      </Layout>
     </>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
