4fce84c6de3b29ba211728c6d173b24346efa6cd
AnujNegi007 <anujnegi708@gmail.com>
2025-06-13 15:15:43 +0530

    implement login endpoint

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,15 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router.tsx'
 import "antd/dist/reset.css"
 import { ConfigProvider } from 'antd'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ConfigProvider theme={{
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider theme={{
       token: {
         colorPrimary: '#F65F42',
         colorLink: '#F65F42'
@@ -16,6 +21,7 @@ createRoot(document.getElementById('root')!).render(
     }}>
       <RouterProvider router={router}/>
     </ConfigProvider>
+    </QueryClientProvider>
     
   </StrictMode>,
 )
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,63 +1,127 @@
-import { Checkbox, Form, Input, Layout, Space, Button, Flex } from "antd";
+import {
+  Checkbox,
+  Form,
+  Input,
+  Layout,
+  Space,
+  Button,
+  Flex,
+  Alert,
+} from "antd";
 import { Card } from "antd";
 import { LockFilled, UserOutlined, LockOutlined } from "@ant-design/icons";
 import Logo from "../../components/icons/Logo";
+import { useMutation } from "@tanstack/react-query";
+import type { Credentials } from "../../types";
+import { login } from "../../http/api";
+
+const loginUser = async (credentials: Credentials) => {
+  const { data } = await login(credentials);
+  return data;
+};
 
 function LoginPage() {
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationKey: ["login"],
+    mutationFn: loginUser,
+    onSuccess: async () => {
+      console.log("Login successfull!");
+    },
+  });
+
   return (
     <>
       <Layout
         style={{ height: "100vh", display: "grid", placeItems: "center" }}
       >
         <Space direction="vertical" align="center" size="large">
-          <Layout.Content style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <Logo/>
-        </Layout.Content>
-        <Card
-          title={
-            <Space
-              style={{ width: "100%", fontSize: 16, justifyContent: "center" }}
+          <Layout.Content
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Logo />
+          </Layout.Content>
+          <Card
+            title={
+              <Space
+                style={{
+                  width: "100%",
+                  fontSize: 16,
+                  justifyContent: "center",
+                }}
+              >
+                <LockFilled />
+                Sign in
+              </Space>
+            }
+            variant="borderless"
+            style={{ width: 300 }}
+          >
+            <Form
+              initialValues={{ remember: true }}
+              onFinish={(values) =>
+                mutate({ email: values.username, password: values.password })
+              }
             >
-              <LockFilled />
-              Sign in
-            </Space>
-          }
-          variant="borderless"
-          style={{ width: 300 }}
-        >
-          <Form initialValues={{ "remember": true}}>
-            <Form.Item name="username" rules={[
-              {
-                required: true,
-                message: 'Please enter username'
-              },{
-                type: 'email',
-                message: 'Email is not valid'
-              }
-            ]}>
-              <Input prefix={<UserOutlined />} placeholder="username" />
-            </Form.Item>
-            <Form.Item name="password" rules={[
-              {
-                required: true,
-                message: 'Please enter password'
-              }
-            ]}>
-              <Input.Password prefix={<LockOutlined />} placeholder="password" />
-            </Form.Item>
-            <Flex justify="space-between">
-              <Form.Item name="remember" valuePropName="checked">
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-              <a href="" id="login-form-forgot">Forgot password</a>
-            </Flex>
-            <Form.Item>
-              <Button type='primary' htmlType="submit" style={{ width: '100%'}}>
-                Login
-              </Button>
-            </Form.Item>
-          </Form>
-        </Card>
+              {isError && (
+                <Alert
+                  type="error"
+                  message={error.message}
+                  style={{ marginBottom: 24 }}
+                />
+              )}
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please enter username",
+                  },
+                  {
+                    type: "email",
+                    message: "Email is not valid",
+                  },
+                ]}
+              >
+                <Input prefix={<UserOutlined />} placeholder="username" />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please enter password",
+                  },
+                ]}
+              >
+                <Input.Password
+                  prefix={<LockOutlined />}
+                  placeholder="password"
+                />
+              </Form.Item>
+              <Flex justify="space-between">
+                <Form.Item name="remember" valuePropName="checked">
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+                <a href="" id="login-form-forgot">
+                  Forgot password
+                </a>
+              </Flex>
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ width: "100%" }}
+                  loading={isPending}
+                >
+                  Login
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
         </Space>
       </Layout>
     </>
