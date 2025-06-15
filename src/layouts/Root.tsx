5bb66fe01c205dd3c85a509f9d11dba0bfe78710
AnujNegi007 <anujnegi708@gmail.com>
2025-06-15 11:11:06 +0530

    persist auth of refresh

---
+++ src/layouts/Root.tsx
@@ -0,0 +1,33 @@
+import { Outlet } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { self } from "../http/api";
+import { useAuthStore } from "../store";
+import { useEffect } from "react";
+
+const getSelf = async () => {
+  const { data } = await self();
+  return data;
+};
+
+function Root() {
+  const { setUser } = useAuthStore();
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["self"],
+    queryFn: getSelf,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setUser(data);
+    }
+  }, [data, setUser]);
+
+  if (isLoading) {
+    return <div>Loading....</div>;
+  }
+
+  return <Outlet />;
+}
+
+export default Root;
