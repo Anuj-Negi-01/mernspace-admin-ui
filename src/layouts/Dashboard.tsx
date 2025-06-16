337ff992cc9c221557f89fe16a480cbf091b9de8
AnujNegi007 <anujnegi708@gmail.com>
2025-06-16 09:44:01 +0530

    add logout functionality

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,7 +1,7 @@
 import { Navigate, NavLink, Outlet } from "react-router-dom";
 import { useAuthStore } from "../store";
-import { Layout, Menu, theme } from "antd";
-import Icon from "@ant-design/icons";
+import { Badge, Flex, Layout, Menu, theme, Space, Dropdown, Avatar } from "antd";
+import Icon, { BellFilled } from "@ant-design/icons";
 import { useState } from "react";
 import Logo from "../components/icons/Logo";
 import Home from "../components/icons/Home";
@@ -9,46 +9,55 @@ import UserIcon from "../components/icons/UserIcon";
 import FoodIcon from "../components/icons/foodIcon";
 import BasketIcon from "../components/icons/BasketIcon";
 import GiftIcon from "../components/icons/GiftIcon";
+import { useMutation } from "@tanstack/react-query";
+import { logout } from "../http/api";
 
 const { Header, Content, Sider, Footer } = Layout;
 
-
 const items = [
   {
-    key: '/',
-    icon: <Icon component={Home}/>,
-    label: <NavLink to="/">Home</NavLink>
+    key: "/",
+    icon: <Icon component={Home} />,
+    label: <NavLink to="/">Home</NavLink>,
   },
   {
-    key: '/users',
+    key: "/users",
     icon: <Icon component={UserIcon} />,
-    label: <NavLink to="/users">Users</NavLink>
+    label: <NavLink to="/users">Users</NavLink>,
   },
   {
-    key: '/restaurants',
+    key: "/restaurants",
     icon: <Icon component={FoodIcon} />,
-    label: <NavLink to="/restaurants">Restaurants</NavLink>
+    label: <NavLink to="/restaurants">Restaurants</NavLink>,
   },
   {
-    key: '/products',
+    key: "/products",
     icon: <Icon component={BasketIcon} />,
-    label: <NavLink to="/products">Products</NavLink>
+    label: <NavLink to="/products">Products</NavLink>,
   },
   {
-    key: '/promos',
+    key: "/promos",
     icon: <Icon component={GiftIcon} />,
-    label: <NavLink to="/promos">Promos</NavLink>
-  }
-]
+    label: <NavLink to="/promos">Promos</NavLink>,
+  },
+];
 
 function Dashboard() {
-  const { user } = useAuthStore();
+  const { user, logout: logoutFromStore } = useAuthStore();
 
-  const [collapsed, setCollapsed] = useState(false)
+  const [collapsed, setCollapsed] = useState(false);
 
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  const { mutate: logoutMutate } = useMutation({
+    mutationKey: ['logout'],
+    mutationFn: logout,
+    onSuccess: async () => {
+      logoutFromStore()
+    }
+  })
 
   if (user === null) {
     return <Navigate to="/auth/login" replace={true} />;
@@ -73,13 +82,42 @@ function Dashboard() {
           />
         </Sider>
         <Layout>
-          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Header
+            style={{
+              paddingLeft: 20,
+              paddingRight: 20,
+              background: colorBgContainer,
+            }}
+          >
+            <Flex gap="middle" align="start" justify="space-between">
+              <Badge text="Admin" status="success" />
+              <Space size={20}>
+                <Badge dot={true}>
+                  <BellFilled />
+                </Badge>
+                <Dropdown menu={{ items: [{
+                    key: '/',
+                    label: 'Logout',
+                    onClick: () => logoutMutate()
+                  }] }} placement="bottomRight" arrow>
+                    <Avatar style={{
+                      background: "#0096FF",
+                      color: "#fff"
+                    }}>
+                      {
+                        user && (
+                          user.firstname.charAt(0)
+                        )
+                      }
+                    </Avatar>
+                  </Dropdown>
+              </Space>
+            </Flex>
+          </Header>
           <Content style={{ margin: "0 16px" }}>
             <Outlet />
           </Content>
-          <Footer style={{ textAlign: "center" }}>
-           Menspace  pizza shop
-          </Footer>
+          <Footer style={{ textAlign: "center" }}>Menspace pizza shop</Footer>
         </Layout>
       </Layout>
     </div>
