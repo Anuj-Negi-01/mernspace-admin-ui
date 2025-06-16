dd0a6df510ba9175ae45e884822c1890f44bc401
AnujNegi007 <anujnegi708@gmail.com>
2025-06-16 09:04:09 +0530

    add sidebar layout

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,17 +1,89 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, NavLink, Outlet } from "react-router-dom";
 import { useAuthStore } from "../store";
+import { Layout, Menu, theme } from "antd";
+import Icon from "@ant-design/icons";
+import { useState } from "react";
+import Logo from "../components/icons/Logo";
+import Home from "../components/icons/Home";
+import UserIcon from "../components/icons/UserIcon";
+import FoodIcon from "../components/icons/foodIcon";
+import BasketIcon from "../components/icons/BasketIcon";
+import GiftIcon from "../components/icons/GiftIcon";
+
+const { Header, Content, Sider, Footer } = Layout;
+
+
+const items = [
+  {
+    key: '/',
+    icon: <Icon component={Home}/>,
+    label: <NavLink to="/">Home</NavLink>
+  },
+  {
+    key: '/users',
+    icon: <Icon component={UserIcon} />,
+    label: <NavLink to="/users">Users</NavLink>
+  },
+  {
+    key: '/restaurants',
+    icon: <Icon component={FoodIcon} />,
+    label: <NavLink to="/restaurants">Restaurants</NavLink>
+  },
+  {
+    key: '/products',
+    icon: <Icon component={BasketIcon} />,
+    label: <NavLink to="/products">Products</NavLink>
+  },
+  {
+    key: '/promos',
+    icon: <Icon component={GiftIcon} />,
+    label: <NavLink to="/promos">Promos</NavLink>
+  }
+]
 
 function Dashboard() {
-    const { user } = useAuthStore();
-    if(user === null){
-        return <Navigate to="/auth/login" replace={true}/>
-    }
+  const { user } = useAuthStore();
+
+  const [collapsed, setCollapsed] = useState(false)
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  if (user === null) {
+    return <Navigate to="/auth/login" replace={true} />;
+  }
   return (
     <div>
-        <h1>This Page is </h1>
-    <Outlet />
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+          theme="light"
+        >
+          <div className="logo">
+            <Logo />
+          </div>
+          <Menu
+            theme="light"
+            defaultSelectedKeys={["/"]}
+            mode="inline"
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Outlet />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+           Menspace  pizza shop
+          </Footer>
+        </Layout>
+      </Layout>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
