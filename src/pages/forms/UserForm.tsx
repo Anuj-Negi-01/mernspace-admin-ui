0910ad9a681328c24fd1a5a8c281dc1e44eedbe9
AnujNegi007 <anujnegi708@gmail.com>
2025-06-18 10:49:00 +0530

    add user form validation

--- src/pages/forms/UserForm.tsx
+++ src/pages/forms/UserForm.tsx
@@ -4,88 +4,146 @@ import { getTenants } from "../../http/api";
 import type { Tenant } from "../../types";
 
 function UserForm() {
-
   const { data: tenants } = useQuery({
     queryKey: ["tenants"],
     queryFn: () => {
-      return getTenants().then((res) => res.data)
-    }
-  })
+      return getTenants().then((res) => res.data);
+    },
+  });
 
   return (
     <Row>
       <Col span={24}>
         <Space direction="vertical" size="large">
           <Card title="Basic info">
-          <Row gutter={20}>
-            <Col span={12}>
-              <Form.Item label="First Name" name="firstname">
-                <Input placeholder="firstname" size="large"></Input>
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item label="Last Name" name="lastname">
-                <Input placeholder="lastname" size="large"></Input>
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-            <Form.Item label="Email" name="email">
-                <Input placeholder="jhon.doe@mernspace.com" type="email" size="large"></Input>
-            </Form.Item>
-            </Col>
-          </Row>
-        </Card>
-        <Card title="Security info">
-            <Row>
-              <Col span={12}>
-                  <Form.Item label="Paasword" name="password">
-                      <Input.Password placeholder="password" size="large" />
-                  </Form.Item>
-              </Col>
-            </Row>
-        </Card>
-        <Card title="Role info">
             <Row gutter={20}>
               <Col span={12}>
-                  <Form.Item label="Role" name="role">
-                    <Select
-                    size="large"
-                  style={{ width: '100%'}}
-                  allowClear={true}
-                  placeholder="Select Role"
-                  >
-                    <Select.Option value="admin">
-                    Admin
-                  </Select.Option>
-                  <Select.Option value="manager">
-                    Manager
-                  </Select.Option>
-                  <Select.Option value="customer">
-                    Customer
-                  </Select.Option>
-                  </Select>
-                  </Form.Item>
+                <Form.Item
+                  label="First Name"
+                  name="firstname"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Firstname is required",
+                    },
+                  ]}
+                >
+                  <Input placeholder="firstname" size="large" />
+                </Form.Item>
               </Col>
               <Col span={12}>
-                <Form.Item label="Restaurant" name="tenantId">
-                    <Select
-                    size="large"
-                  style={{ width: '100%'}}
-                  allowClear={true}
-                  placeholder="Select Restaurant"
-                  >
+                <Form.Item
+                  label="Last Name"
+                  name="lastname"
+                  rules={[
                     {
-                      tenants && tenants.map((tenant: Tenant) => (
-                        <Select.Option key={tenant.id} value={tenant.id}>
-                          { tenant.name }
-                        </Select.Option>
-                      ))
-                    }
-                  </Select>
-                  </Form.Item>
+                      required: true,
+                      message: "Lastname is required",
+                    },
+                  ]}
+                >
+                  <Input placeholder="lastname" size="large" />
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item
+                  label="Email"
+                  name="email"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Email is required",
+                    },
+                    {
+                      type: "email",
+                      message: "Email is not valid",
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="jhon.doe@mernspace.com"
+                    type="email"
+                    size="large"
+                  />
+                </Form.Item>
               </Col>
             </Row>
-        </Card>
+          </Card>
+          <Card title="Security info">
+            <Row>
+              <Col span={12}>
+                <Form.Item
+                  label="Paasword"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Password is required",
+                    },
+                    {
+                      len: 8,
+                      message: 'Password must be more than 7 chars'
+                    }
+                  ]}
+                >
+                  <Input.Password placeholder="password" size="large" />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
+          <Card title="Role info">
+            <Row gutter={20}>
+              <Col span={12}>
+                <Form.Item
+                  label="Role"
+                  name="role"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Role is required",
+                    },
+                  ]}
+                >
+                  <Select
+                    size="large"
+                    style={{ width: "100%" }}
+                    allowClear={true}
+                    placeholder="Select Role"
+                  >
+                    <Select.Option value="admin">Admin</Select.Option>
+                    <Select.Option value="manager">Manager</Select.Option>
+                    <Select.Option value="customer">Customer</Select.Option>
+                  </Select>
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item
+                  label="Restaurant"
+                  name="tenantId"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Restaurant is required",
+                    },
+                  ]}
+                >
+                  <Select
+                    size="large"
+                    style={{ width: "100%" }}
+                    allowClear={true}
+                    placeholder="Select Restaurant"
+                  >
+                    {tenants &&
+                      tenants.map((tenant: Tenant) => (
+                        <Select.Option key={tenant.id} value={tenant.id}>
+                          {tenant.name}
+                        </Select.Option>
+                      ))}
+                  </Select>
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
         </Space>
       </Col>
     </Row>
